836d7e5b5ef96b37cca04377733f0d4481bad6bb
Evgeny <evgeny.efimenko@proton.me>
2024-04-04 20:49:00 +0500

    feat: tremor components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,16 @@
-const DashboardPage = () => {
-  return <p>Dashboard</p>
+import { AnalyticsDashboard } from "@/components/analytics-dashboard"
+import { analytics } from "@/utils/analytics"
+
+const DashboardPage = async () => {
+  const pageView = await analytics.retrieveDays("pageview", 2)
+
+  return (
+    <div className="min-h-screen w-full py-12 flex justify-center items-center">
+      <div className="relative w-full max-w-6xl mx-auto">
+        <AnalyticsDashboard />
+      </div>
+    </div>
+  )
 }
 
 export default DashboardPage
--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,5 +1,6 @@
 import { redis } from "@/lib/redis"
 import { getDate } from "@/utils"
+import { parse } from "date-fns"
 
 interface AnalyticsArgs {
   retention?: number
@@ -25,6 +26,43 @@ export class Analytics {
 
     if (!opts?.persist) {
       await redis.expire(key, this.retention)
+    }
+  }
+
+  async retrieveDays(namespace: string, nDays: number) {
+    type analyticsPrimise = ReturnType<typeof analytics.retrieve>
+    const promises: analyticsPrimise[] = []
+
+    for (let i = 0; i < nDays; i++) {
+      const formattedDate = getDate(i)
+      const promise = analytics.retrieve(namespace, formattedDate)
+      promises.push(promise)
+    }
+    const fetched = await Promise.all(promises)
+    const data = fetched.sort((a, b) => {
+      if (
+        parse(a.date, "dd/MM/yyyy", new Date()) >
+        parse(b.date, "dd/MM/yyyy", new Date())
+      ) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+
+    return data
+  }
+
+  async retrieve(namespace: string, date: string) {
+    const res = await redis.hgetall<Record<string, string>>(
+      `analytics::${namespace}::${date}`
+    )
+
+    return {
+      date,
+      events: Object.entries(res ?? []).map(([key, value]) => ({
+        [key]: Number(value),
+      })),
     }
   }
 }
